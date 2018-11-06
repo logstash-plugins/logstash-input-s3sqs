1661858a4b7a72ff4647c08194417e5624e4143f
Rob Bavey <rob.bavey@elastic.co>
2018-11-06 14:11:06 -0500

    Adds a unit test
    Adds the standard "behaves like an interruptible input plugin" test

--- spec/inputs/s3sqs_spec.rb
+++ spec/inputs/s3sqs_spec.rb
@@ -1,8 +1,23 @@
 # encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
 require "logstash/inputs/s3sqs"
+require 'aws-sdk'
 
 describe LogStash::Inputs::S3SQS do
 
+  context 'an s3sqs plugin' do
+    let (:poller) { double('poller')}
+    before :each do
+      allow(subject).to receive(:setup_queue)
+      allow(subject).to receive(:poller).and_return poller
+      allow(poller).to receive(:before_request).and_return Aws::SQS::QueuePoller::PollerStats.new
+      allow(poller).to receive(:poll) do |_|
+        sleep(2)
+      end
+    end
 
+    it_behaves_like "an interruptible input plugin" do
+      let(:config) { { "queue" => 'queue'} }
+    end
+  end
 end
